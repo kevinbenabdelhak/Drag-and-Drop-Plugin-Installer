e244fb954dc208fb3cdb57ba1ebc828b891ed575
Kevin Benabdelhak <163947458+kevinbenabdelhak@users.noreply.github.com>
2024-07-11 01:40:47 +0200

    Update Drag-&-Drop-Plugin-Installer.php

--- Drag-&-Drop-Plugin-Installer.php
+++ Drag-&-Drop-Plugin-Installer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Drag & Drop Plugin Installer
  * Plugin URI: https://kevin-benabdelhak.fr/plugins/drag-and-drop-plugin-installer/
- * Description: Drag & Drop Plugin Installer permet d'installer et d'activuer une ou plusieurs extension WordPress facilement depuis n'importe quelle page de l'administration.
+ * Description: Drag & Drop Plugin Installer permet d'installer et d'activer une ou plusieurs extension WordPress facilement depuis n'importe quelle page de l'administration.
  * Author: Kevin BENABDELHAK
  * Author URI: https://kevin-benabdelhak.fr/a-propos/
  */
